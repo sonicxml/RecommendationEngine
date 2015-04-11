d78fc5d139d47ef63932ab5c2b12fd6551053e75
sonicxml <sonicxml@gmail.com>
2015-04-10 20:26:37 -0400

    Add a Graph class
    just to get some source files in here

---
+++ src/Graph.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class Graph {
+    private int numNodes;
+
+    // Hash from <src, <tgt, weight>>
+    private HashMap<Integer, HashMap<Integer, Integer>> adjList;
+
+    public Graph() {
+        // Howdy mate
+    }
+}
