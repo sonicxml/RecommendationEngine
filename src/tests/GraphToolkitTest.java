3e491358e0fef205db483d5f4d56022da0c22810
DeepanS206 <deepan.saravanan83@gmail.com>
2015-04-24 18:18:56 -0400

    added test for bfs: test passes

--- src/tests/GraphToolkitTest.java
+++ src/tests/GraphToolkitTest.java
@@ -1,7 +1,10 @@
 package tests;
 
+import java.util.*; 
 import org.junit.Before;
+import static org.junit.Assert.*;
 import org.junit.Test;
+import engine.*; 
 
 public class GraphToolkitTest {
 
@@ -12,7 +15,20 @@ public class GraphToolkitTest {
 
     @Test
     public void testBfs() throws Exception {
-
+    	Graph g = DataReader.readSampleGraphData("TestGraphs/bfs_acyclic.txt"); 
+    	Node src = g.getNodeByID(1); 
+    	Node tgt = g.getNodeByID(4); 
+    	List<Node> nodes = GraphToolkit.bfs(g, src, tgt, false); 
+    	List<Integer> path = new LinkedList<Integer>(); 
+    	for (Node n : nodes) {
+    		path.add(n.getID()); 
+    	}
+    	List<Integer> answer = new LinkedList<Integer>(); 
+    	answer.add(1); 
+    	answer.add(2); 
+    	answer.add(4); 
+    	assertEquals("see if output is shortest path", path, answer); 
+    	
     }
 
     @Test
